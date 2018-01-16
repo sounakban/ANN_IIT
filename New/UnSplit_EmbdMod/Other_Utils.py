117164aed323b9ce9d82b9b4347bc575c2ce3d20
Sounak Banerjee <sounakban.science@example.com>
2018-01-16 21:54:53 +0530

    Sync

--- New/UnSplit_EmbdMod/Other_Utils.py
+++ New/UnSplit_EmbdMod/Other_Utils.py
@@ -2,13 +2,22 @@
 import numpy as np
 
 def prob2Onehot(prob_dist):
-	import numpy as np
 	ret_vec = []
 	for probs in prob_dist:
 		probs_list = list(probs)
 		temp = [0]*len(probs_list)
 		temp[probs_list.index(max(probs_list))] = 1
 		ret_vec.append(temp)
+	return np.array(ret_vec)
+
+def prob2Onehot3D(prob_dist):
+	ret_vec = []
+	for elem in prob_dist:
+		for probs in elem:
+			probs_list = list(probs)
+			temp = [0]*len(probs_list)
+			temp[probs_list.index(max(probs_list))] = 1
+			ret_vec.append(temp)
 	return np.array(ret_vec)
 
 
