03b396e1773557493feeab7dc7a4bd12dee63c15
Sounak Banerjee <sounakban.science@example.com>
2018-01-17 16:13:20 +0530

    Sync

--- New/UnSplit_EmbdMod/BiLSTM_Keras.py
+++ New/UnSplit_EmbdMod/BiLSTM_Keras.py
@@ -63,9 +63,9 @@ seq = Bidirectional(LSTM(256, dropout=0.5, recurrent_dropout=0.2, return_sequenc
 #seq = Bidirectional(LSTM(256, dropout=0.5, return_sequences=True), merge_mode='concat')(embed_layer)
 #seq = Bidirectional(LSTM(256, return_sequences=True), merge_mode='concat')(embed_layer)
 #seq = Dropout(0.5)(seq)
-embedding_len = np.shape(embed_layer)[-1]
-print("Shape of input sequence : ", np.shape(seq))
-mlp = TimeDistributed(Dense(3, activation='softmax', input_shape=(69, embedding_len)))(seq)
+softmax_input_len = np.shape(seq)[-1]
+print("Shape of input sequence : ", softmax_input_len)
+mlp = TimeDistributed(Dense(3, activation='softmax', input_shape=(69, softmax_input_len)))(seq)
 model = Model(inputs=[word_inp, POS_inp], outputs=mlp)
 optimizer = Adam(lr=0.001)
 model.compile(optimizer=optimizer, loss='categorical_crossentropy')
