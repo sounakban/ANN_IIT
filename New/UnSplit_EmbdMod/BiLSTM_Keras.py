67c179b648d89a87753f824b1fb3a0d5c9f5a382
Sounak Banerjee <sounakban.science@example.com>
2018-01-18 17:45:16 +0530

    Sync

--- New/UnSplit_EmbdMod/BiLSTM_Keras.py
+++ New/UnSplit_EmbdMod/BiLSTM_Keras.py
@@ -68,15 +68,15 @@ print("Shape, total embd: ", np.shape(embed_layer))
 # print("Shape of Bi-LSTM output : ", np.shape(seq))
 
 
-forwards = LSTM(64, return_sequences=True, activation='tanh')(embed_layer)
-backwards = LSTM(64, return_sequences=True, go_backwards=True, activation='tanh')(embed_layer)
+forwards = LSTM(256, return_sequences=True, activation='tanh')(embed_layer)
+backwards = LSTM(256, return_sequences=True, go_backwards=True, activation='tanh')(embed_layer)
 seq = Concatenate(axis=-1)([forwards, backwards])
 
 
 print(np.shape(seq))
 seq = Dropout(0.5)(seq)
 seq = Concatenate(axis=-1)([seq, POS_embed_layer])
-# mlp = TimeDistributed(Dense(256, activation='relu'))(seq)
+mlp = TimeDistributed(Dense(256, activation='relu'))(seq)
 mlp = TimeDistributed(Dense(128, activation='relu'))(seq)
 mlp = TimeDistributed(Dense(64, activation='relu'))(seq)
 mlp = TimeDistributed(Dense(3, activation='softmax'))(seq)
